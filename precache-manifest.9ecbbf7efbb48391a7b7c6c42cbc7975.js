30d79144037225603c3aa111afe0652a45008b7d
Magnus <magnus.e@outlook.com>
2020-06-10 03:19:43 +0200

    Updates

---
+++ precache-manifest.9ecbbf7efbb48391a7b7c6c42cbc7975.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6717c4b68b776e059de11dce78e5fa6d",
+    "url": "/random-quote-generator/index.html"
+  },
+  {
+    "revision": "40aff9339ad80064d379",
+    "url": "/random-quote-generator/static/css/main.9fa6eb73.chunk.css"
+  },
+  {
+    "revision": "484318a814a988379161",
+    "url": "/random-quote-generator/static/js/2.07ed6ed9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/random-quote-generator/static/js/2.07ed6ed9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40aff9339ad80064d379",
+    "url": "/random-quote-generator/static/js/main.748761af.chunk.js"
+  },
+  {
+    "revision": "0574d6473bdf7da0b4a0",
+    "url": "/random-quote-generator/static/js/runtime-main.08ac65e2.js"
+  }
+]);
