04f059c4fb97d8c24633e25d12ff9b5f95e4f1a9
Magnus <magnus.e@outlook.com>
2020-06-10 04:12:50 +0200

    First

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
+var quotes = [
+  {
+    text: "Life is short, eat lasagna",
+    author: "Me"
+  },
+  {
+    text: "If your stomach little empty, your head little empty",
+    author: "Mr. Lee (rip)"
+  },
+  {
+    text: "I like turtles",
+    author: "Some kid"
+  },
+  {
+    text: "Ten people died last night in the Bronx due to a fire that killed ten people in the Bronx last night during a fire",
+    author: "The news"
+  }
+]
+
 function App() {
+  let [quote, setQuote] = useState({
+    "text": "",
+    "author": ""
+  })
+
+  function newQuote() {
+    const quoteNumber = Math.floor(Math.random() * quotes.length)
+    const newQuote = quotes[quoteNumber]
+    setQuote(newQuote)
+    return
+  }
+  useEffect(() => {
+    newQuote()
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <div id={"quote-box"}>
+          <h1 id={"text"}>{quote.text}</h1>
+          <p id={"author"}>{quote.author}</p>
+          <div id={"buttons"}>
+            <button onClick={newQuote} id={"new-quote"}>New Quote please</button>
+            <a id={"tweet-quote"}
+               href={"https://twitter.com/intent/tweet?text=" + quote.text + "&url=nymo.xyz"}>Tweet this</a>
+          </div>
+        </div>
+      </div>
   );
 }
 
